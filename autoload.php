170889ccb48cf1d7c74479aa41cdf05e4a13553e
Bernardo <bernardo@magdalenamedio.net>
2012-08-14 21:35:25 -0500

    Signed-off-by: Bernardo <bernardo@magdalenamedio.net>

--- autoload.php
+++ autoload.php
@@ -1,29 +1,29 @@
 <?php
-	define ("PATHS", $paths); 
-	function __autoload($class_name) 
+define ("PATHS", $paths); 
+function __autoload($class_name) 
+{
+	$class_name = strtolower($class_name);
+	if(PATHS)
+		$classPath = explode(',', PATHS);
+	else
+		$classPath = array();
+		
+	foreach($classPath as $prefix)
 	{
-		$class_name = strtolower($class_name);
-		if(PATHS)
-			$classPath = explode(',', PATHS);
-		else
-			$classPath = array();
-			
-		foreach($classPath as $prefix)
+		$extensions = unserialize(FILE_EXTENSIONS);
+		
+		$prefix = ($prefix != '') ? $prefix . '/' : '';
+		
+		
+		foreach($extensions as $extension)
 		{
-			$extensions = unserialize(FILE_EXTENSIONS);
-			
-			$prefix = ($prefix != '') ? $prefix . '/' : '';
-			
-			
-			foreach($extensions as $extension)
+			$path = $prefix . $class_name . $extension . '.php';
+			if(file_exists($path))
 			{
-				$path = $prefix . $class_name . $extension . '.php';
-				if(file_exists($path))
-				{
-					require_once $path;
-					return;
-				}
+				require_once $path;
+				return;
 			}
-		}  
-	} 
+		}
+	}  
+} 
 ?>
